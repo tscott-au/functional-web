383495fd9245cde310e5dbbd9a139282cd22c9cf
Tim Scott <tim@nepelle.com.au>
2018-08-20 16:05:10 +1000

    added island and base tests

--- lib/islands_engine/coordinate.ex
+++ lib/islands_engine/coordinate.ex
@@ -1,3 +1,11 @@
+#---
+# Excerpted from "Functional Web Development with Elixir, OTP, and Phoenix",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/lhelph for more book information.
+#---
 defmodule IslandsEngine.Coordinate do
   alias __MODULE__
 
--- test/guesses_test.exs
+++ test/guesses_test.exs
@@ -1,13 +1,14 @@
 defmodule GuessesTest do
   use ExUnit.Case
+  alias IslandsEngine.{Coordinate, Guesses}
   ## doctest Guesses
 
 
   test "Test guesses" do
-    guesses = IslandsEngine.Guesses.new
-    {:ok, coordinate1} = IslandsEngine.Coordinate.new(1, 1)
-    {:ok, coordinate2} = IslandsEngine.Coordinate.new(2, 2)
-    {:ok, coordinate3} = IslandsEngine.Coordinate.new(1, 1)
+    guesses = Guesses.new
+    {:ok, coordinate1} = Coordinate.new(1, 1)
+    {:ok, coordinate2} = Coordinate.new(2, 2)
+    {:ok, coordinate3} = Coordinate.new(1, 1)
     guesses = update_in(guesses.hits, &MapSet.put(&1, coordinate1))
     guesses = update_in(guesses.hits, &MapSet.put(&1, coordinate2))
     guesses = update_in(guesses.hits, &MapSet.put(&1, coordinate3))
