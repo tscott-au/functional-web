2f12ffa47fd849f45365629d65e473e46400a43f
Tim Scott <tim@nepelle.com.au>
2018-09-19 18:17:25 +1000

    Set up game gen server and tests

---
+++ lib/islands_engine/game.ex
@@ -0,0 +1,139 @@
+#---
+# Excerpted from "Functional Web Development with Elixir, OTP, and Phoenix",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/lhelph for more book information.
+#---
+defmodule IslandsEngine.Game do
+  use GenServer
+
+  alias IslandsEngine.{Board, Coordinate, Guesses, Island, Rules}
+
+  @players [:player1, :player2]
+
+  def start_link(name) when is_binary(name), do:
+    GenServer.start_link(__MODULE__, name, [])
+
+  def init(name) do
+    player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
+    player2 = %{name: nil,  board: Board.new(), guesses: Guesses.new()}
+    {:ok, %{player1: player1, player2: player2, rules: %Rules{}}}
+  end
+
+  def add_player(game, name) when is_binary(name), do:
+    GenServer.call(game, {:add_player, name})
+
+  def position_island(game, player, key, row, col) when player in @players, do:
+    GenServer.call(game, {:position_island, player, key, row, col})
+
+  def set_islands(game, player) when player in @players, do:
+    GenServer.call(game, {:set_islands, player})
+
+  def guess_coordinate(game, player, row, col) when player in @players, do:
+    GenServer.call(game, {:guess_coordinate, player, row, col})
+
+
+  def handle_call({:add_player, name}, _from, state_data) do
+    with {:ok, rules} <- Rules.check(state_data.rules, :add_player)
+    do
+      state_data
+      |> update_player2_name(name)
+      |> update_rules(rules)
+      |> reply_success(:ok)
+    else
+      :error -> {:reply, :error, state_data}
+      {:error, _msg} -> {:reply, :error, state_data}
+    end
+  end
+
+  def handle_call({:position_island, player, key, row, col}, _from, state_data)
+  do
+    board = player_board(state_data, player)
+    with  {:ok, rules} <-
+            Rules.check(state_data.rules, {:position_islands, player}),
+          {:ok, coordinate} <-
+            Coordinate.new(row, col),
+          {:ok, island} <-
+            Island.new(key, coordinate),
+          %{} = board <-
+            Board.position_island(board, key, island)
+    do
+      state_data
+      |> update_board(player, board)
+      |> update_rules(rules)
+      |> reply_success(:ok)
+    else
+      :error -> {:reply, :error, state_data}
+      {:error, :invalid_coordinate} ->
+        {:reply, {:error, :invalid_coordinate}, state_data}
+      {:error, :invalid_island_type} ->
+        {:reply, {:error, :invalid_island_type}, state_data}
+      {:error, msg} -> {:reply, {:error, msg}, state_data}
+    end
+  end
+
+  def handle_call({:set_islands, player}, _from, state) do
+    board = player_board(state, player)
+    with {:ok, rules} <- Rules.check(state.rules, {:set_islands, player}),
+      true <- Board.all_islands_positioned?(board)
+    do
+      state
+      |> update_rules(rules)
+      |> reply_success({:ok, board})
+    else
+      :error -> {:reply, :error, state}
+      false -> {:reply, :not_all_islands_positioned, state}
+      {:error, _msg} -> {:reply, :error, state}
+    end
+  end
+
+  def handle_call({:guess_coordinate, player, row, col}, _from, state) do
+    opponent = get_opponent(player)
+    board = player_board(state, opponent)
+
+    with {:ok, rules} <- Rules.check(state.rules, {:guess_coordinate, player}),
+      {:ok, coordinate} <- Coordinate.new(row, col),
+      {hit_or_miss, forested_island, win_status, board} <- Board.guess(board, coordinate),
+      {:ok, rules} <- Rules.check(rules, {:win_check, win_status})
+    do
+      state
+      |> update_board(opponent, board)
+      |> update_guesses(player, hit_or_miss, coordinate)
+      |> update_rules(rules)
+      |> reply_success({hit_or_miss, forested_island, win_status})
+    else
+      :error -> {:reply, :error, state}
+      {:error, :invalid_coordinate} ->
+        {:reply, {:error, :invalid_coordinate}, state}
+
+      {:error, _msg} -> {:reply, :error, state}
+    end
+  end
+
+
+  defp update_player2_name(state_data, name), do:
+    put_in(state_data.player2.name, name)
+
+  defp update_rules(state_data, rules), do: %{state_data | rules: rules}
+
+  defp update_board(state_data, player, board), do:
+    Map.update!(state_data, player, fn player -> %{player | board: board} end)
+
+  defp update_guesses(state_data, player, hit_or_miss, coordinate) do
+    update_in(state_data[player].guesses, fn guesses ->
+      Guesses.add(guesses, hit_or_miss, coordinate)
+    end)
+  end
+
+
+  defp reply_success(state_data, reply), do: {:reply, reply, state_data}
+
+  defp player_board(state_data, player), do: Map.get(state_data, player).board
+
+  defp get_opponent(:player1), do: :player2
+
+  defp get_opponent(:player2), do: :player1
+
+end
---
+++ test/game_test.exs
@@ -0,0 +1,184 @@
+defmodule GameTest do
+  use ExUnit.Case
+  alias IslandsEngine.{Game,  Island,  Coordinate}
+
+  test "init" do
+    {:ok, game } = Game.start_link("fred")
+    state = :sys.get_state(game)
+    assert state.player1.name == "fred"
+
+  end
+
+  test "player2" do
+    {:ok, game } = Game.start_link("fred")
+    Game.add_player(game, "betty")
+
+    state  = :sys.get_state(game)
+    assert state.player2.name == "betty"
+
+  end
+
+
+  test "position a island" do
+    {:ok, game } = Game.start_link("fred")
+    Game.add_player(game, "betty")
+    error = Game.position_island(game, :player1, :square, 1, 1)
+    assert error == :ok
+    state = :sys.get_state(game)
+    c = state.player1.board.square.coordinates
+    assert MapSet.member?(c, %IslandsEngine.Coordinate{col: 1, row: 1})
+  end
+
+  defp assert_state(game, state) do
+    s = :sys.get_state(game)
+    assert s.rules.state == state
+  end
+
+  test "states" do
+    {:ok, game } = Game.start_link("fred")
+    assert_state(game, :initialized)
+
+    Game.add_player(game, "betty")
+    assert_state(game, :players_set)
+
+    add_islands(game, :player2, :pattern1)
+    assert_state(game, :players_set)
+
+    add_islands(game, :player1, :pattern1)
+    assert_state(game, :players_set)
+
+    Game.position_island(game, :player1, :square, 1, 3)
+    assert_state(game, :players_set)
+
+    Game.set_islands(game, :player1)
+    assert_state(game, :players_set)
+
+    Game.set_islands(game, :player2)
+    assert_state(game, :player1_turn)
+
+
+    # state = :sys.replace_state(game, fn state_data -> %{state_data | rules: %Rules{state: :player1_turn}} end)
+    # assert state.rules.state == :player1_turn
+
+    # assert {:error, "default"} == Game.position_island(game, :player1, :dot, 10, 1 )
+
+  end
+
+  test "position islands" do
+    {:ok, game } = Game.start_link("fred")
+    Game.add_player(game, "betty")
+    error = Game.position_island(game, :player1, :square, 1, 1)
+    assert error == :ok
+    error = Game.position_island(game, :player1, :square, 2, 2)
+    assert error == :ok
+    error = Game.position_island(game, :player1, :square, 4, 3)
+    assert error == :ok
+    error = Game.position_island(game, :player1, :square, 4, 4)
+    assert error == :ok
+    state = :sys.get_state(game)
+    c = state.player1.board.square.coordinates
+    assert MapSet.member?(c, %IslandsEngine.Coordinate{col: 4, row: 4})
+    assert Map.size(state.player1.board) == 1
+  end
+
+
+  test "set islands" do
+    game = new_game()
+    add_islands(game, :player1, :pattern1)
+
+    error = Game.set_islands(game, :player2)
+    assert error == :not_all_islands_positioned
+    add_islands(game, :player2, :pattern1)
+
+    {error, _board} = Game.set_islands(game, :player1)
+    assert error == :ok
+
+    {error, _board} = Game.set_islands(game, :player1)
+    assert error == :ok
+
+  end
+
+
+
+  test "guess" do
+    game = new_game()
+    add_islands(game, :player1, :pattern1)
+    add_islands(game, :player2, :pattern1)
+    set_islands(game, :player1)
+    set_islands(game, :player2)
+
+    error = Game.guess_coordinate(game, :player1, 1, 3)
+    assert error == {:miss, :none, :no_win}
+
+    error = Game.guess_coordinate(game, :player2, 1, 1)
+    assert error == {:hit, :none, :no_win}
+
+    error = Game.guess_coordinate(game, :player2, 1, 2)
+    assert error == :error
+
+    error = Game.guess_coordinate(game, :player1, 5, 3)
+    assert error == {:hit, :none, :no_win}
+
+  end
+
+  test "win" do
+    game = new_game()
+    add_islands(game, :player1, :pattern1)
+    add_islands(game, :player2, :pattern1)
+    set_islands(game, :player1)
+    set_islands(game, :player2)
+    %Coordinate{row: row, col: col} = almost_win(game)
+
+    {_hit_mis, _island, win} = Game.guess_coordinate(game, :player1, row, col)
+    assert win == :win
+
+
+  end
+
+
+
+  defp new_game() do
+    {:ok, game } = Game.start_link("fred")
+    error = Game.add_player(game, "betty")
+    assert error == :ok
+    game
+  end
+
+  defp add_islands(game, player, :pattern1) do
+    error = Game.position_island(game, player, :square, 1, 1)
+    assert error == :ok
+    error = Game.position_island(game, player, :dot, 10, 1)
+    assert error == :ok
+    error = Game.position_island(game, player, :atoll, 5, 3)
+    assert error == :ok
+    error = Game.position_island(game, player, :l_shape, 7, 1)
+    assert error == :ok
+    error = Game.position_island(game, player, :s_shape, 9, 8)
+    assert error == :ok
+  end
+
+  defp set_islands(game, player) do
+    {error, _board} = Game.set_islands(game, player)
+    assert error == :ok
+  end
+
+  defp almost_win(game) do
+    {player1_last, coords} = get_coords(game, :player1)
+      |> List.pop_at(0)
+
+    Enum.each(coords, fn %Coordinate{row: row, col: col} ->
+      Game.guess_coordinate(game, :player1, row, col )
+      Game.guess_coordinate(game, :player2, 1, 1 )
+      end )
+    player1_last
+  end
+
+  defp get_coords(game, player) do
+    state = :sys.get_state(game)
+    Map.get(state, player).board
+    |> Map.values
+    |> Enum.map(fn %Island{coordinates: c, hit_coordinates: _h} -> MapSet.to_list(c) end)
+    |> List.flatten
+  end
+
+end
